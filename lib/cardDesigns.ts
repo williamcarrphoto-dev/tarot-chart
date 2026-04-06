3b2392e3516e6e6e3f98bc52e14cf45d8c1e213e
williamcarrphoto-dev <williamcarr.photo@gmail.com>
2026-04-06 20:17:15 +1000

    Add all 12 zodiac sign card designs

--- lib/cardDesigns.ts
+++ lib/cardDesigns.ts
@@ -1,9 +1,20 @@
 export const CARD_DESIGNS = [
-  { id: 'card-01', name: 'Cosmic Purple', image: require('../assets/Card Designs/card-01.png') },
+  { id: 'aries', name: 'Aries', image: require('../assets/Card Designs/Aries.png') },
+  { id: 'taurus', name: 'Taurus', image: require('../assets/Card Designs/Taurus.png') },
+  { id: 'gemini', name: 'Gemini', image: require('../assets/Card Designs/Gemini.png') },
+  { id: 'cancer', name: 'Cancer', image: require('../assets/Card Designs/Cancer.png') },
+  { id: 'leo', name: 'Leo', image: require('../assets/Card Designs/Leo.png') },
+  { id: 'virgo', name: 'Virgo', image: require('../assets/Card Designs/Virgo.png') },
+  { id: 'libra', name: 'Libra', image: require('../assets/Card Designs/Libra.png') },
+  { id: 'scorpio', name: 'Scorpio', image: require('../assets/Card Designs/Scorpio.png') },
+  { id: 'sagittarius', name: 'Sagittarius', image: require('../assets/Card Designs/Sagittarius.png') },
+  { id: 'capricorn', name: 'Capricorn', image: require('../assets/Card Designs/Capricorn.png') },
+  { id: 'aquarius', name: 'Aquarius', image: require('../assets/Card Designs/Aquarius.png') },
+  { id: 'pisces', name: 'Pisces', image: require('../assets/Card Designs/Pisces.png') },
 ];
 
-// Default card design for all users
-export const DEFAULT_CARD_DESIGN = 'card-01';
+// Default card design for all users (Aries as first sign)
+export const DEFAULT_CARD_DESIGN = 'aries';
 
 export function getCardDesign(id: string | undefined) {
   // Use default if no ID provided
